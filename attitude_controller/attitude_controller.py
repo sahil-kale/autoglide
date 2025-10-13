026915141c630b012437e17c0cbe11f1291f0b5e
Sahil Kale <sahil.kale1@gmail.com>
2025-10-12 20:18:53 -0700

    Add in attitude controller file

---
+++ attitude_controller/attitude_controller.py
@@ -0,0 +1,16 @@
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from vehicle_interface import vehicle_interface
+from dataclasses import dataclass
+
+@dataclass
+class AttitudeControllerInputs:
+    roll_ref_rad: float = 0.0  # desired roll angle, +ve = roll right
+    pitch_ref_rad: float = 0.0  # desired pitch angle, +ve = pitch up
+
+class AttitudeController:
+    def __init__(self):
+        # todo: set up gains?
+        pass
