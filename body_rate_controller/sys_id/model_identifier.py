cf81faa3e22d69968afd36750c010b92350fd880
Sahil Kale <sahil.kale1@gmail.com>
2025-11-16 18:10:05 -0800

    first pass model identifer (note module rename)

--- body_rate_controller/sys_id/model_identifier.py
+++ body_rate_controller/sys_id/model_identifier.py
@@ -1,170 +1,151 @@
-import click
-from dataclasses import dataclass
-from enum import Enum, auto
+from jsbsim_sandbox.sandbox_sim import (
+    JSBSimVehicleConfig,
+    JSBSimVehicleInitialCond,
+    JSBSimSimParams,
+    JSBSim_Sandbox,
+)
+
+from utils.pid import PIDConfig
+from jsbsim_sandbox.vehicle_state_visualizer import animate_sim
+import numpy as np
+
 from body_rate_controller.sys_id.trim_controller import (
-    TrimConfig,
     GliderAttitudeTrimController,
+    TrimConfig,
     TrimTarget,
 )
-from jsbsim_sandbox.sandbox_sim import JSBSim_Sandbox
-from utils.pid import PIDConfig
-import numpy as np
-from body_rate_controller.sys_id.single_axis_perturber import (
-    SingleAxisPerturber,
-    SingleAxisPerturberEvent,
-    SingleAxisPerturberType,
+
+from body_rate_controller.sys_id.body_rate_model_perturber import (
+    BodyRateModelPerturber,
 )
-from vehicle_interface.vehicle_interface import ControlCommands
+import click
 
 
-class BodyRateModelPerturberAxes(Enum):
-    ROLL = auto()
-    PITCH = auto()
-    YAW = auto()
+class BodyRateModelIdentifier:
+    def __init__(self, sim_truth_state_history: list):
+        self.sim_truth_state_history = sim_truth_state_history
 
+    def identify_model(self):
+        click.secho("Starting model identification...", fg="blue")
+        state_at_t0 = self.sim_truth_state_history[0]
 
-class BodyRateModelPerturber:
-    def __init__(
-        self,
-        sim: JSBSim_Sandbox,
-        trim: GliderAttitudeTrimController,
-        trim_target: TrimTarget,
-    ):
-        self.sim = sim
-        self.trim = trim
-        self.trim_target = trim_target
+        time_vector = []
+        p_radps_vector = []
+        q_radps_vector = []
+        r_radps_vector = []
+        aileron_deflection_vector = []
+        elevator_deflection_vector = []
+        rudder_deflection_vector = []
 
-        # Roll and Yaw use the same generic profile.
-        generic_axis_perturbation_magnitude = 0.4
-        self.roll_perturbation_config = [
-            # +Step
-            SingleAxisPerturberEvent(
-                event_type=SingleAxisPerturberType.STEP,
-                magnitude=generic_axis_perturbation_magnitude,
-                duration_s=0.5,
-            ),
-            SingleAxisPerturberEvent(
-                event_type=SingleAxisPerturberType.STEP,
-                magnitude=0.0,
-                duration_s=0.5,
-            ),
-            # -Step
-            SingleAxisPerturberEvent(
-                event_type=SingleAxisPerturberType.STEP,
-                magnitude=-generic_axis_perturbation_magnitude,
-                duration_s=0.5,
-            ),
-            SingleAxisPerturberEvent(
-                event_type=SingleAxisPerturberType.STEP,
-                magnitude=generic_axis_perturbation_magnitude,
-                duration_s=1.0,
-            ),
-        ]
-
-        elevator_perturbation_magnitude = 0.4
-        self.pitch_perturbation_config = [
-            SingleAxisPerturberEvent(
-                event_type=SingleAxisPerturberType.STEP,
-                magnitude=-elevator_perturbation_magnitude,
-                duration_s=0.25,
-            ),
-            SingleAxisPerturberEvent(
-                event_type=SingleAxisPerturberType.STEP,
-                magnitude=0.0,
-                duration_s=0.25,
-            ),
-            SingleAxisPerturberEvent(
-                event_type=SingleAxisPerturberType.STEP,
-                magnitude=elevator_perturbation_magnitude,
-                duration_s=0.5,
-            ),
-            SingleAxisPerturberEvent(
-                event_type=SingleAxisPerturberType.STEP,
-                magnitude=elevator_perturbation_magnitude * 0.5,
-                duration_s=0.5,
-            ),
-        ]
-
-        # Yaw uses the same generic profile as roll.
-        self.yaw_perturbation_config = list(self.roll_perturbation_config)
-
-    def run(self):
-        for axis in BodyRateModelPerturberAxes:
-            perturber = self._get_perturber_for_axis(axis)
-            click.secho(
-                f"Starting perturbation sequence for axis: {axis.name}", fg="blue"
+        for state in self.sim_truth_state_history:
+            time_vector.append(state.time_s - state_at_t0.time_s)
+            p_radps_vector.append(state.p_radps - state_at_t0.p_radps)
+            q_radps_vector.append(state.q_radps - state_at_t0.q_radps)
+            r_radps_vector.append(state.r_radps - state_at_t0.r_radps)
+            aileron_deflection_vector.append(
+                state.control_commands.aileron_deflection_norm
+                - state_at_t0.control_commands.aileron_deflection_norm
             )
-            perturber.start(current_time_s=self.sim.get_sim_time_s())
-
-            while perturber.active:
-                perturbation = perturber.step(current_time_s=self.sim.get_sim_time_s())
-                control_commands = self._get_control_command_from_perturbation(
-                    perturbation, axis
-                )
-                self.sim.step(control_commands=control_commands)
-
-            click.secho(
-                f"Completed perturbation sequence for axis: {axis.name}", fg="green"
+            elevator_deflection_vector.append(
+                state.control_commands.elevator_deflection_norm
+                - state_at_t0.control_commands.elevator_deflection_norm
+            )
+            rudder_deflection_vector.append(
+                state.control_commands.rudder_deflection_norm
+                - state_at_t0.control_commands.rudder_deflection_norm
             )
 
-            self.trim.reset()
-            self.trim.run_until_trim(
-                trim_target=self.trim_target,
+        # Convert lists to numpy arrays for easier manipulation
+        time_vector = np.array(time_vector)
+        p_radps_vector = np.array(p_radps_vector)
+        q_radps_vector = np.array(q_radps_vector)
+        r_radps_vector = np.array(r_radps_vector)
+        aileron_deflection_vector = np.array(aileron_deflection_vector)
+        elevator_deflection_vector = np.array(elevator_deflection_vector)
+        rudder_deflection_vector = np.array(rudder_deflection_vector)
+
+        """
+        Structure of model to identify:
+        Let omega = [p, q, r] (body rates).
+        Let delta = [aileron, elevator, rudder] (control deflections).
+        Let k be the time step index, and N be the total number of time steps minus 2.
+
+        We want to find:
+        omega_{k+1} = A * omega_k + B * delta_k
+
+        Using least squares, we can stack the data to solve for A and B.
+        Let Phi = [omega, delta] (becomes a N x 6 matrix).
+        Let Y = [omega_next] (becomes a N x 3 matrix), where omega_next is omega shifted by one time step.
+        Let Theta = [A; B] (Where A, B are both 3x3 matrices, stacked vertically to form a 6x3 matrix).
+        We want to solve Y_est = Phi * Theta, formally stated as:
+        argmin || Y - Phi * Theta ||^2 over Theta
+        """
+        omega = np.vstack((p_radps_vector, q_radps_vector, r_radps_vector)).T
+        omega_next = omega[1:, :]
+        # remove the last element inside omega
+        omega = omega[:-1, :]
+        expected_omega_shape = (len(time_vector) - 1, 3)
+        self._verify_array_shape(omega, expected_omega_shape, "Omega")
+
+        expected_omega_next_shape = (len(time_vector) - 1, 3)
+        self._verify_array_shape(omega_next, expected_omega_next_shape, "Omega Next")
+
+        delta = np.vstack(
+            (
+                aileron_deflection_vector,
+                elevator_deflection_vector,
+                rudder_deflection_vector,
             )
-        click.secho("Completed all axis perturbation sequences.", fg="green", bold=True)
+        ).T
+        delta = delta[:-1, :]
+        expected_delta_shape = (len(time_vector) - 1, 3)
+        self._verify_array_shape(delta, expected_delta_shape, "Delta")
 
-    def _get_perturber_for_axis(
-        self, axis: BodyRateModelPerturberAxes
-    ) -> SingleAxisPerturber:
-        match axis:
-            case BodyRateModelPerturberAxes.ROLL:
-                return SingleAxisPerturber(events=self.roll_perturbation_config)
-            case BodyRateModelPerturberAxes.PITCH:
-                return SingleAxisPerturber(events=self.pitch_perturbation_config)
-            case BodyRateModelPerturberAxes.YAW:
-                return SingleAxisPerturber(events=self.yaw_perturbation_config)
-            case _:
-                raise ValueError(
-                    click.secho(
-                        f"Unknown perturber axis: {axis}",
-                        fg="red",
-                        bold=True,
-                        err=True,
-                    )
+        Phi = np.hstack((omega, delta))  # N x 6
+        Y = omega_next  # N x 3
+
+        if Phi.shape[0] != Y.shape[0]:
+            raise RuntimeError(
+                click.secho(
+                    f"Phi and Y row count mismatch. Phi rows: {Phi.shape[0]}, Y rows: {Y.shape[0]}",
+                    fg="red",
+                    bold=True,
                 )
+            )
 
-    def _get_control_command_from_perturbation(
-        self, perturbation: float, axis: BodyRateModelPerturberAxes
-    ) -> ControlCommands:
-        control_commands = self.trim.sim_truth_state_history[-1].control_commands
-        match axis:
-            case BodyRateModelPerturberAxes.PITCH:
-                control_commands.elevator_deflection_norm += perturbation
-            case BodyRateModelPerturberAxes.ROLL:
-                control_commands.aileron_deflection_norm += perturbation
-            case BodyRateModelPerturberAxes.YAW:
-                control_commands.rudder_deflection_norm += perturbation
+        # Solve for Theta using least squares
+        Theta, residuals, rank, s = np.linalg.lstsq(Phi, Y, rcond=None)
+        A = Theta[:3, :].T  # 3x3
+        B = Theta[3:, :].T  # 3x3
+        click.secho("Identified model parameters:", fg="green")
+        click.secho(f"A matrix:\n{A}", fg="green")
+        click.secho(f"B matrix:\n{B}", fg="green")
 
-            case _:
-                raise ValueError(
-                    click.secho(
-                        f"Unknown perturber axis: {axis}",
-                        fg="red",
-                        bold=True,
-                        err=True,
-                    )
+        # Fit stats
+        Y_est = Phi @ Theta  # N x 3
+        residuals = Y - Y_est
+        ss_res = np.sum(residuals**2, axis=0)
+        ss_tot = np.sum((Y - np.mean(Y, axis=0)) ** 2, axis=0)
+        r_squared = 1 - (ss_res / ss_tot)
+        click.secho(f"R-squared values for each body rate:", fg="green")
+        click.secho(
+            f"p: {r_squared[0]:.4f}, q: {r_squared[1]:.4f}, r: {r_squared[2]:.4f}",
+            fg="green",
+        )
+
+    @staticmethod
+    def _verify_array_shape(array, expected_shape, array_name):
+        if array.shape != expected_shape:
+            raise RuntimeError(
+                click.secho(
+                    f"{array_name} shape mismatch. Expected {expected_shape}, got {array.shape}",
+                    fg="red",
+                    bold=True,
                 )
-
-        return control_commands
+            )
 
 
 if __name__ == "__main__":
-    from jsbsim_sandbox.sandbox_sim import (
-        JSBSimVehicleConfig,
-        JSBSimVehicleInitialCond,
-        JSBSimSimParams,
-    )
-
     initial_cond = JSBSimVehicleInitialCond(
         h0_m=1000.0,
         vt0_mps=30.0,
@@ -204,8 +185,8 @@ if __name__ == "__main__":
     )
 
     trim_target = TrimTarget(
-        roll_rad=np.deg2rad(0),
-        pitch_rad=np.deg2rad(0),
+        roll_rad=np.deg2rad(30),
+        pitch_rad=np.deg2rad(-10),
         sideslip_rad=np.deg2rad(0),
     )
 
@@ -222,6 +203,5 @@ if __name__ == "__main__":
     idx_of_perturb_start = time_at_perturb_start_s // sim_params.dt_s
     history = history[int(idx_of_perturb_start) :]
 
-    from jsbsim_sandbox.vehicle_state_visualizer import animate_sim
-
-    animate_sim(history, interval_ms=0.005, frame_step=10)
+    model_identifier = BodyRateModelIdentifier(history)
+    model_identifier.identify_model()
